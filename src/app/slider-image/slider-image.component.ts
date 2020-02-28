ab258baa860c5db6b0918b8240a5133b52234b8b
Vincent <vdterpst@gmail.com>
2020-02-28 12:20:45 -0500

    Image slider allows for one image to partially overlay another

--- src/app/slider-image/slider-image.component.ts
+++ src/app/slider-image/slider-image.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-slider-image',
   templateUrl: './slider-image.component.html',
   styleUrls: ['./slider-image.component.css']
 })
-export class SliderImageComponent implements OnInit {
+export class SliderImageComponent {
 
+  width = 200;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(){
+    
   }
 
+  onMouseMove($event: MouseEvent){
+    if($event.buttons == 1)
+      this.setWidth($event.clientX - document.getElementById("slider").offsetLeft);
+  }
+
+  setWidth(_width: number){
+    this.width = Math.max(0, Math.min(_width, 400));
+  }
 }
